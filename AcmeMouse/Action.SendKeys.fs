32fb1e61b0d35cad781df94a7b2de030ab6d885c
Matthew Goodall <dogsaw@thecyberplains.com>
2013-10-07 08:44:00 +1300

    Actually add the sendkeys part
    Guess I missed it in the earlier commit

---
+++ AcmeMouse/Action.SendKeys.fs
@@ -0,0 +1,22 @@
+﻿module SendKeys
+
+open PInvoke.Methods
+open PInvoke.Types
+
+open System.Runtime.InteropServices
+
+let SendKeys (keys: List<VK>) pos =
+    if keys.Length = 0 then
+        ()
+    else        
+        // I have been having uses using SendInput to send keyup, so I have switched to the slightly worse keybd_event for now
+    
+        // Send the keys
+        for key in keys do        
+            keybd_event(key, 0u, KEYEVENTF.NONE, 0n)
+            
+        // and their corresponding key up (don't want to get into ctrl lock...)
+        for key in keys do            
+            keybd_event(key, 0u, KEYEVENTF.KEYUP, 0n)
+        
+        ()
