bc2b30e1f3f1d0d6ecce2bdb0543a45e4a8263de
Matthew Goodall <dogsaw@thecyberplains.com>
2013-10-07 08:43:25 +1300

    Clean up main program method

--- AcmeMouse/Program.fs
+++ AcmeMouse/Program.fs
@@ -4,54 +4,24 @@
 open MouseHandler
 open MouseChord
 
-open PInvoke.Types
-
-let han a b c =
-     printfn "%A %A %A" a b c 
-     PInvoke.Types.LRESULT.Zero
+//open PInvoke.Types
 
 let error = System.Runtime.InteropServices.Marshal.GetLastWin32Error()
 
 [<EntryPoint>]
-let main argv = 
-(*
-    let mh = new MouseHook(PInvoke.Methods.LowLevelMouseProc(fun a b c -> printfn "%A %A %A" a b c
-                                                                          0n))
-                     
-                     
-                                                                                                  
-                                                            *)
-                                        
+let main argv =                                        
     printfn "started"
-
+(*
     let mouseEvent = new Event<string>()
     use eventedHook = new LowLevelMouseHook(fun nCode wParam lParam ->
         mouseEvent.Trigger(sprintf "%A %A (x:%A y:%A d:%A f:%A t:%A)" nCode wParam lParam.pt.x lParam.pt.y lParam.mouseData lParam.flags lParam.time) |> ignore
         true
     )
     mouseEvent.Publish.Subscribe(fun (str: string) -> System.Console.WriteLine str) |> ignore
-
+*)
     let chorder = new ChordedMouseHook()
-
-
-    (*
-    use noRight = new LowLevelMouseHook(fun nCode wParam lParam ->
-        if wParam = WM.RBUTTONDOWN || wParam = WM.RBUTTONUP then
-            false
-        else
-            true
-    )
-    *)
     printfn "hooked"
 
     System.Windows.Forms.Application.Run()
 
-    (*
-    let message = PInvoke.Types.MSG()
-    while PInvoke.Methods.GetMessage(message, 0n, 0u, 0u) do
-        printfn "!%A" error
-        *)
-
-    System.Console.ReadLine() |> ignore
-    printfn "%A" argv
     0 // return an integer exit code
