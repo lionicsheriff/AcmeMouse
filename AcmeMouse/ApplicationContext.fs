f696a65cd2ce05046caed831415d7e66528fdfe1
Matthew Goodall <dogsaw@thecyberplains.com>
2013-10-08 14:29:12 +1300

    Add simple help message

--- AcmeMouse/ApplicationContext.fs
+++ AcmeMouse/ApplicationContext.fs
@@ -5,6 +5,18 @@ open System.Resources
 open System.Reflection
 open MouseChord
 
+let ShowHelp = fun sender e -> MessageBox.Show(([ "Press the mouse buttons indicated"
+                                                  "in one motion (i.e without releasing"
+                                                  "any buttons)"
+                                                  ""
+                                                  "L->M: Ctrl+X"
+                                                  "L->M->R: Ctrl+C"
+                                                  "L->R: Ctrl+V"
+                                                  "L->R->M: cancel"
+                                                ] |> String.concat System.Environment.NewLine),
+                                                "AcmeMouse")
+                                |> ignore
+
 type AcmeMouseContext() =
     inherit ApplicationContext()
     let resources = new ResourceManager("Resources",Assembly.GetExecutingAssembly())
@@ -14,10 +26,13 @@ type AcmeMouseContext() =
     let OnExit args =
         trayIcon.Visible <- false
         (hook :> System.IDisposable).Dispose ()
-    do  
+    do
         trayIcon.Icon <- resources.GetObject "AppIcon" :?> System.Drawing.Icon
         trayIcon.Text <- "AcmeMouse"
-        trayIcon.ContextMenu <- new ContextMenu([|new MenuItem("E&xit", new System.EventHandler(fun sender e -> Application.Exit()))|])
+        trayIcon.ContextMenu <- new ContextMenu([|
+            new MenuItem("&Help", new System.EventHandler(ShowHelp));
+            new MenuItem("E&xit", new System.EventHandler(fun sender e -> Application.Exit()));
+            |])
         trayIcon.Visible <- true
         System.Console.WriteLine "context created"
         Application.ApplicationExit.Add (OnExit)
