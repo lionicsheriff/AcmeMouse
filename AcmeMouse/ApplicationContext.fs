ecfddb9f4d6bc0c01686d9d5dc6d4ad0a90bbb04
Matthew Goodall <dogsaw@thecyberplains.com>
2013-11-05 12:05:11 +1300

    Re-organise application context
    May move the handlers out of the application class later, but for now it I
    want to have the handlers consistent.

--- AcmeMouse/ApplicationContext.fs
+++ AcmeMouse/ApplicationContext.fs
@@ -5,16 +5,6 @@ open System.Resources
 open System.Reflection
 open MouseChord
 
-let ShowHelp = fun sender e -> MessageBox.Show(([ "Press the mouse buttons in one motion"
-                                                  "(i.e without releasing any buttons)"
-                                                  ""
-                                                  "L->M: Ctrl+X"
-                                                  "L->M->R: Ctrl+C"
-                                                  "L->R: Ctrl+V"
-                                                  "L->R->M: cancel"
-                                                ] |> String.concat System.Environment.NewLine),
-                                                "AcmeMouse")
-                                |> ignore
 
 let menuItem text handler config =
     let item = new MenuItem(text, new System.EventHandler(handler))
@@ -27,28 +17,39 @@ let menuItem text handler config =
 
 type AcmeMouseContext() =
     inherit ApplicationContext()
-    let resources = new ResourceManager("Resources",Assembly.GetExecutingAssembly())
-    let components = new System.ComponentModel.Container()
-    let trayIcon = new NotifyIcon(components)
-    let hook = new ChordedMouseHook()
+    let resources = new ResourceManager("Resources",Assembly.GetExecutingAssembly ())
+    let components = new System.ComponentModel.Container ()
+    let trayIcon = new NotifyIcon (components)
+    let hook = new ChordedMouseHook ()
     let toggleMouseWheel (sender: obj) e =
         hook.BlockWheel <- not hook.BlockWheel
         let menuItem = sender :?> MenuItem
         menuItem.Checked <- hook.BlockWheel
+
+    let showHelp = fun sender e -> MessageBox.Show(([ "Press the mouse buttons in one motion"
+                                                      "(i.e without releasing any buttons)"
+                                                      ""
+                                                      "L->M: Ctrl+X"
+                                                      "L->M->R: Ctrl+C"
+                                                      "L->R: Ctrl+V"
+                                                      "L->R->M: cancel"
+                                                    ] |> String.concat System.Environment.NewLine),
+                                                    "AcmeMouse")
+                                    |> ignore
         
-    let OnExit args =
+    let onExit args =
         trayIcon.Visible <- false
         (hook :> System.IDisposable).Dispose ()
     do
         trayIcon.Icon <- resources.GetObject "AppIcon" :?> System.Drawing.Icon
         trayIcon.Text <- "AcmeMouse"
         trayIcon.ContextMenu <- new ContextMenu([|
-                                                menuItem "&Help" ShowHelp None;
+                                                menuItem "&Help" showHelp None;
                                                 menuItem "Block Mouse &Wheel" toggleMouseWheel (Some(fun item -> item.Checked <- hook.BlockWheel));
                                                 menuItem "E&xit" (fun s e -> Application.Exit ()) None;
                                                 |])
         trayIcon.Visible <- true
         System.Console.WriteLine "context created"
-        Application.ApplicationExit.Add (OnExit)
+        Application.ApplicationExit.Add onExit
     member this.TrayIcon = trayIcon
     member this.Hook = hook
